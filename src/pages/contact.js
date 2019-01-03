4e1c0010da91a6f86e679f977eb7c78cbe5231fe
Dreyah <dreyah.a@gmail.com>
2019-01-03 19:50:23 +0100

    create multiple pages and layout component
    create an index page, about page, and a contact page.
    also create a layout component that centers content on page including global navigation so it’s easy for visitors to find and visit each of the sub-pages.

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <div>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:dreyah.a@gmail.com">dreyah.a@gmail.com</a>
+      </p>
+    </div>
+  </Layout>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>
+      Hey! I'm Dreyah, and I'm building a Gatsby site as part of a tutorial!
+    </h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
+);
